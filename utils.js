a02cc6540301ca5f965d4bc824dadea6cbf4e31b
Semion <fv.sqn.mad@gmail.com>
2023-12-10 17:13:10 +0100

    updated utils with math gcd lcm functions

--- utils.js
+++ utils.js
@@ -12,4 +12,19 @@ const readInputFile = (fileName) => {
 
 const splitByLine = (file) => file.split(/\r?\n/);
 
-module.exports = { readInputFile, splitByLine };
+// Greatest Common Divisor
+const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+
+// Least Common Multiple
+const lcm = (a, b) => (a * b) / gcd(a, b);
+
+// LCM for array
+const findLCM = (numbers) => {
+  let result = 1;
+  for (let i = 0; i < numbers.length; i++) {
+    result = lcm(result, numbers[i]);
+  }
+  return result;
+};
+
+module.exports = { readInputFile, splitByLine, findLCM };
